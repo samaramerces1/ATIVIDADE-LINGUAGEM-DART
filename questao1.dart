c687559621e51888d2a0e3e187d02fbc220b29a0
Samara Mercês <154234561+samaramerces1@users.noreply.github.com>
2026-02-25 10:17:08 -0300

    Questão-01

---
+++ questao1.dart
@@ -0,0 +1,11 @@
+void main() {
+  String nomeProduto = "Notebook";
+  double preco = 3500.00;
+  int quantidade = 5;
+  bool disponivel = true;
+
+  print("Nome do Produto: $nomeProduto");
+  print("Preço: R\$ $preco");
+  print("Quantidade em estoque: $quantidade");
+  print("Disponível para venda: $disponivel");
+}
